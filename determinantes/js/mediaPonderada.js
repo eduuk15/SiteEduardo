51862023e311de0205bcecd512375745b343974e
Eduardo <eduardoknopp7@gmail.com>
2022-04-20 15:35:15 -0300

    feat: adiciona botão e guarda valores do input

--- determinantes/js/mediaPonderada.js
+++ determinantes/js/mediaPonderada.js
@@ -1,10 +1,17 @@
-function calculaMedia() {
-    var notas = document.getElementsByClassName("nota");
-    console.log(notas);
-
-}
-
 function insereNota() {
     var form = document.querySelector("form");
-    form.insertAdjacentHTML('beforeend', '<div><input id="nota" type="text" placeholder="Insira sua nota aqui"><input id="peso" type="text" placeholder="Insira o peso da prova"></div>');
+    form.insertAdjacentHTML('beforeend', '<div><input class="nota" type="text" placeholder="Insira sua nota aqui"><input class="peso" type="text" placeholder="Insira o peso da prova"></div>');
+}
+
+function calculaMedia() {
+    // var notas = document.querySelector(".nota").value;
+    // console.log(notas);
+
+    let nota = document.querySelectorAll(".nota");
+
+        let notas = [].map.call(nota, function (input) {
+          return input.value;
+        })
+
+        console.log(notas);
 }
