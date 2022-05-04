45d3bb2bf046f7728cfadd996b02b19c9acd610f
Eduardo <eduardoknopp7@gmail.com>
2022-05-04 11:46:51 -0300

    refactor: melhoria de código e css

--- determinantes/js/medias/mediaPonderada.js
+++ determinantes/js/medias/mediaPonderada.js
@@ -1,12 +1,12 @@
-function insereNota() {
+function insereValor() {
     let form = document.querySelector("form");
-    form.insertAdjacentHTML('beforeend', '<div><input class="nota" type="text" placeholder="Insira sua nota aqui"><input class="peso" type="text" placeholder="Insira o peso da prova"></div>');
+    form.insertAdjacentHTML('beforeend', '<div><input class="valor" type="number" placeholder="Insira o valor aqui"><input class="peso" type="number" placeholder="Insira o peso aqui"</div>');
 }
 
 function calculaMedia() {
 
-    let nota = document.querySelectorAll(".nota");
-    let notas = [].map.call(nota, function (input) {
+    let valor = document.querySelectorAll(".valor");
+    let valores = [].map.call(valor, function (input) {
       return parseFloat(input.value);
     });
 
@@ -15,14 +15,14 @@ function calculaMedia() {
       return parseFloat(input.value);
     });
 
-    let somaNotas = 0
+    let somaValores = 0
     let somaPesos = 0;
-    for (let i = 0; i < notas.length; i++) {
-        somaNotas += notas[i] * pesos[i];
+    for (let i = 0; i < valores.length; i++) {
+        somaValores += valores[i] * pesos[i];
         somaPesos += pesos[i]
     }
 
-    let resultado = somaNotas / somaPesos;
+    let resultado = somaValores / somaPesos;
 
     if (!resultado) {
       resultado = 'Não foi possível realizar o cálculo...';
