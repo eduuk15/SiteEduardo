5f996dc167e2c2a497b07847f72d04c3a541c51b
Eduardo <eduardoknopp7@gmail.com>
2022-05-05 12:48:16 -0300

    feat: adiciona tratamento do coeficiente e o cálculo do resultado

--- SiteEduardo/js/equacoes/equacPrimeiro.js
+++ SiteEduardo/js/equacoes/equacPrimeiro.js
@@ -2,6 +2,10 @@ function calculaEquacao() {
     let sinais = ["+", "-"];
     let equacao = document.getElementById("equacao").value;
     let ladosIgualdade = equacao.split(" = ");
+    let alfabeto =  [
+        "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J",
+        "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", 
+        "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"];
 
     ///////////////////////////////////////////////////
     let ladoEsquerdo = ladosIgualdade[0];
@@ -31,10 +35,27 @@ function calculaEquacao() {
         }
     }
 
+    ///////////////////////////////////////////////////
+    let coeficiente = 0;
+    for (let i = 0; i < alfabeto.length; i++) {
+        coeficiente = parseFloat(ladoEsquerdo.split(`${alfabeto[i]}`)[0]);
+        if (isNaN(coeficiente) === true && coeficiente != alfabeto[i]) {
+            coeficiente = 1;
+        }
+    }
+
+    let resultado = (somaElementosLadoDireito - somaElementosLadoEsquerdo) / coeficiente;
+
+    
+
     console.log(equacao);
     console.log(ladosIgualdade);
     console.log(ladoEsquerdo);
     console.log(elementosLadoEsquerdo);
     console.log(ladoDireito);
     console.log(elementosLadoDireito);
+    console.log(somaElementosLadoDireito);
+    console.log(somaElementosLadoEsquerdo);
+    console.log(coeficiente);
+    console.log(resultado);
 }
