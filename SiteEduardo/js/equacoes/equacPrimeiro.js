d0cde61f4e5f5b1dd3a727b2b8850dcb1f014340
Eduardo <eduardoknopp7@gmail.com>
2022-05-09 23:41:55 -0300

    fix: retorna ao commit anterior

--- SiteEduardo/js/equacoes/equacPrimeiro.js
+++ SiteEduardo/js/equacoes/equacPrimeiro.js
@@ -2,6 +2,10 @@ function calculaEquacao() {
     let sinais = ["+", "-"];
     let equacao = document.getElementById("equacao").value;
     let ladosIgualdade = equacao.split(" = ");
+    let alfabeto =  [
+        "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J",
+        "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", 
+        "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z"];
 
     ///////////////////////////////////////////////////
     let ladoEsquerdo = ladosIgualdade[0];
@@ -31,10 +35,43 @@ function calculaEquacao() {
         }
     }
 
-    console.log(equacao);
-    console.log(ladosIgualdade);
-    console.log(ladoEsquerdo);
-    console.log(elementosLadoEsquerdo);
-    console.log(ladoDireito);
-    console.log(elementosLadoDireito);
+    ///////////////////////////////////////////////////
+    let incognita = "";
+    let coeficiente = 0;
+    for (let i = 0; i < alfabeto.length; i++) {
+        if (ladoEsquerdo.includes(alfabeto[i])) {
+            incognita = alfabeto[i];
+        }
+    }
+
+    for (let i = 0; i < elementosLadoEsquerdo.length; i++) {
+        if (elementosLadoEsquerdo[i].toString().includes(`${incognita}`)) {
+            coeficiente += parseFloat(elementosLadoEsquerdo[i]);
+            console.log('entrou');
+            console.log(elementosLadoEsquerdo[i]);
+            console.log(coeficiente);
+        }
+        if (isNaN(coeficiente) === true && coeficiente !== incognita) {
+            coeficiente = 1;
+        }
+        
+    }
+
+    ///////////////////////////////////////////////////
+    let resultado = (somaElementosLadoDireito - somaElementosLadoEsquerdo) / coeficiente;
+    document.getElementById("resultado").innerHTML = "Resultado: " + incognita + " = " + resultado;
+
+    
+
+    // console.log(equacao); 
+    // console.log(ladosIgualdade); 
+    // console.log(ladoEsquerdo); 
+    // console.log(elementosLadoEsquerdo);
+    // console.log(ladoDireito); 
+    // console.log(elementosLadoDireito); 
+    // console.log(somaElementosLadoDireito); 
+    // console.log(somaElementosLadoEsquerdo); 
+    // console.log(coeficiente); 
+    // console.log(resultado); 
+    // console.log(incognita); 
 }
