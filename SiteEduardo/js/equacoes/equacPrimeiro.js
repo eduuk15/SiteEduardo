df2a714f23ce451da3a792a485d9a1e892384893
Eduardo <eduardo@conectra.com.br>
2022-05-20 13:25:36 -0300

    feat: add os mesmos tratamento do lado esq para o lado dir

--- SiteEduardo/js/equacoes/equacPrimeiro.js
+++ SiteEduardo/js/equacoes/equacPrimeiro.js
@@ -40,11 +40,25 @@ function calculaEquacao() {
 
     ////////////////////////////////////////////////// - Tratamento dos elementos do lado direito da igualdade
     let ladoDireito = ladosIgualdade[1];
+    for (let i = 0; i < alfabeto.length; i++) {
+        if (ladoDireito.includes(alfabeto[i])) {
+            incognita = alfabeto[i];
+        }
+    }
     let elementosLadoDireito = ladoDireito.split(" ");
     let somaElementosLadoDireito = 0;
     for (let i = 0; i < elementosLadoDireito.length; i++) {
         if (elementosLadoDireito[i] === sinais[1]) {
-            elementosLadoDireito[i + 1] *= -1;
+            if (isNaN(elementosLadoDireito[i + 1]) === false) {
+                elementosLadoDireito[i + 1] *= -1;
+            }
+            if (elementosLadoDireito[i + 1].toString().includes(`${incognita}`) && elementosLadoDireito[i + 1] !== incognita) {
+                elementosLadoDireito[i + 1] = (-1 * parseFloat(elementosLadoDireito[i + 1])) + incognita;
+            }
+
+            if (isNaN(elementosLadoDireito[i]) === true && typeof elementosLadoDireito[i] !== 'string') {
+                elementosLadoDireito[i] = -1 + incognita;
+            }
         }
 
         if (isNaN(elementosLadoDireito[i]) === false) {
@@ -77,8 +91,8 @@ function calculaEquacao() {
     // console.log(ladoEsquerdo);
     console.log('Os elementos do lado esquerdo são: ' + elementosLadoEsquerdo);
     // console.log(ladoDireito);
-    // console.log(elementosLadoDireito);
-    // console.log(somaElementosLadoDireito);
+    console.log('Os elementos do lado direito são: ' + elementosLadoDireito);
+    console.log('A soma dos elementos do lado direito é: ' + somaElementosLadoDireito);
     // console.log(somaElementosLadoEsquerdo);
     console.log('O coeficiente é: ' + coeficiente);
     // console.log(resultado);
